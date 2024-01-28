951cf9a2ecf60652c58cc1b7bff3b41e395d5910
mlaouhi <mlaouhi.yahya@gmail.com>
2024-01-28 21:21:37 +0100

    test: add tests to consumer

--- consumer/data_consumer/tests.py
+++ consumer/data_consumer/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import ProcessMessageView, SendMessageView, TaskResultViewSet
 
-# Create your tests here.
+class UrlsTest(SimpleTestCase):
+    def test_send_message_url_resolves(self):
+        url = reverse('send_message')
+        self.assertEqual(resolve(url).func.view_class, SendMessageView)
+
+    def test_process_message_url_resolves(self):
+        url = reverse('process_message')
+        self.assertEqual(resolve(url).func.view_class, ProcessMessageView)
+
+    def test_tasks_url_resolves(self):
+        url = reverse('taskresult-list')  #
+        self.assertEqual(resolve(url).func.cls, TaskResultViewSet)
